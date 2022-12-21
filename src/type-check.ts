4a8cb69ed75379f9d2e7b85baab50e9cb2d53192
Bala Challa <balakrishnachalla@gmail.com>
2022-12-22 01:32:10 +0530

    formatted and linted with lint-staged before commit

--- src/type-check.ts
+++ src/type-check.ts
@@ -2,5 +2,9 @@ function add(a: number, b: number) {
   return a + b;
 }
 
-console.log(add(1, 2));
-//console.log(add(1, 'type'));
+try {
+  console.log(add(1, 2));
+} catch (e) {
+  //e = 10; // won't allow it get committed
+  console.log(e);
+}
